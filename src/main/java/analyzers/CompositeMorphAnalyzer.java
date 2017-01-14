12cc1be4d55739878b55d6a3952e0c3f9dab1df4
EvgeniyHSE <uevgenii@yandex.ru>
2017-01-15 00:23:39 +0300

    Composite analyzer

--- src/main/java/analyzers/CompositeMorphAnalyzer.java
+++ src/main/java/analyzers/CompositeMorphAnalyzer.java
@@ -2,24 +2,58 @@ package analyzers;
 
 
 import datamodel.IWord;
+
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
- * Morphological analyzer which uses other analyzers todo ?
+ * Morphological analyzer which uses other analyzers
  */
 public class CompositeMorphAnalyzer implements IMorphAnalyzer {
 
-    String msg;
-    public CompositeMorphAnalyzer(String message){
-        msg = message;
+    private List<IMorphAnalyzer> analyzers;
+
+    public CompositeMorphAnalyzer(List<IMorphAnalyzer> analyzers){
+
+        // If composite analyzer does not include not null analyzer than throw exception
+        if (analyzers == null || analyzers.size() == 0){
+            final String EXC_MESSAGE = "Constructor must contain at least one Morphological Analyzer";
+            throw new IllegalArgumentException(EXC_MESSAGE);
+        }
+
+        // Add not null factories to ArrayList
+        this.analyzers = new ArrayList<>();
+
+        for (IMorphAnalyzer an : analyzers){
+            if (an != null){
+                this.analyzers.add(an);
+            }
+        }
     }
 
-    public Collection<IWord> analyze(String word) {
-        System.out.println(msg);
-        return null;
+    public Collection<IWord> analyze (String word) {
+
+        // Use the first analyzer which is able to analyze the word
+        for (IMorphAnalyzer an : analyzers) {
+            if (an.canHandle(word)){
+                return an.analyze(word);
+            }
+        }
+
+        // In order to avoid this exception user should check the possibility of analyzer
+        // by invoking canHandle() method
+        final String EXC_MESSAGE = "No analyzer can analyze word \'" + word + "\'";
+        throw new UnsupportedOperationException(EXC_MESSAGE);
     }
+
 
     public Boolean canHandle(String word) {
-        return null;
+        for (IMorphAnalyzer an : analyzers) {
+            if (an.canHandle(word)){
+                return true;
+            }
+        }
+        return false;
     }
 }
